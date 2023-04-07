c2746cc1b5ef7047a1b44d1514121002c0d84558
samir25648 <samir.alanya@tecsup.edu.pe>
2023-04-07 16:44:34 -0500

    Agregar archivos de proyecto.

---
+++ Lab02/Producto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Producto
+{
+    public string Nombre { get; set; }
+    public double Precio { get; set; }
+    public DateTime FechaVencimiento { get; set; }
+    public string Descripcion { get; set; }
+    public bool EstaVencido()
+    {
+        if (DateTime.Now > FechaVencimiento)
+        {
+            Console.WriteLine("El producto está vencido.");
+            return true;
+        }
+        else
+        {
+            Console.WriteLine("El producto no está vencido.");
+            return false;
+        }
+    }
+    public double GetPrecioVenta()
+    {
+        double igv = Precio * 0.18;
+        double precioVenta = Precio + igv;
+        return precioVenta;
+    }
+}
---
+++ Lab02/Program.cs
@@ -0,0 +1,9 @@
+﻿
+Producto producto = new Producto();
+producto.Nombre = "Producto 1";
+producto.Precio = 20.0;
+producto.FechaVencimiento = new DateTime(2023, 04, 06);
+producto.Descripcion = "Descripción del producto 1";
+
+producto.EstaVencido();
+Console.WriteLine("Precio de venta: " + producto.GetPrecioVenta());
